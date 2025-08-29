86a3ea750101aee12143158b3c2ae0bdcb1516a3
kawajan <kawajan2@gmail.com>
2025-08-29 19:58:29 +0200

    get all tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: any[] = [];
+
+    createTask(task: any) {
+        this.tasks.push(task);
+    }
+
+    getAllTasks() {
+        return this.tasks;
+    }
+}
