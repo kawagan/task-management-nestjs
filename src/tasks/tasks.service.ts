f109d5571dc40d164e7f3be969600b5396ab338e
kawajan <kawajan2@gmail.com>
2025-08-30 16:29:20 +0200

    get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,6 +7,10 @@ import { Task, TaskStatus } from './task.model';
 export class TasksService {
   private tasks: Task[] = [];
 
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
@@ -18,8 +22,7 @@ export class TasksService {
     this.tasks.push(task);
     return task;
   }
-
-  getAllTasks(): Task[] {
-    return this.tasks;
+  getTaskById(id: string): Task | undefined {
+    return this.tasks.find(task => task.id === id);
   }
 }
