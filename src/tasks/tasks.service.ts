913beb4b10642087bcca6e583c8809044a760dde
kawajan <kawajan2@gmail.com>
2025-08-30 16:43:33 +0200

    update status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -29,4 +29,12 @@ export class TasksService {
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter(task => task.id !== id);
   }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task | undefined {
+    const task = this.getTaskById(id);
+    if (task) {
+      task.status = status;
+    }
+    return task;
+  }
 }
