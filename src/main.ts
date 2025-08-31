34fe0686b79f5a57b9cb0365baeaeacf3844323d
kawajan <kawajan2@gmail.com>
2025-08-31 11:01:59 +0200

    create validation for create tas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable global validation for all incoming requests using class-validator decorators
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
