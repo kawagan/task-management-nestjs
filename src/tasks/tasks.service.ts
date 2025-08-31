a4eae86ba6b1623091a7e1d194f3d0e7e22cc01f
kawajan <kawajan2@gmail.com>
2025-08-31 11:06:39 +0200

    handle error if task-id not found

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -42,7 +42,11 @@ export class TasksService {
     return task;
   }
   getTaskById(id: string): Task | undefined {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    return found;
   }
 
   deleteTask(id: string): void {
