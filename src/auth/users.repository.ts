6b95805bc3c7ad010b78c48071dc1d503a0a9a3d
kawajan <kawajan2@gmail.com>
2025-09-02 16:53:10 +0200

    create use

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -9,4 +10,10 @@ export class UsersRepository {
     @InjectRepository(User)
     private repository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { email, password } = authCredentialsDto;
+    const user = this.repository.create({ email, password });
+    await this.repository.save(user);
+  }
 }
