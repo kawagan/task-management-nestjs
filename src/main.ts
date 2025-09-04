efcd1d436a2f9ceed6d8ee1d5ac5f9332b4483d2
kawajan <kawajan2@gmail.com>
2025-09-04 18:25:43 +0200

    hide user

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable global validation for all incoming requests using class-validator decorators
   app.useGlobalPipes(new ValidationPipe());
+  // Enable global transformation of responses using class-transformer decorators
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import { User } from 'src/auth/user.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
@@ -18,5 +19,6 @@ export class Task {
 
   // eager: false means whenever we fetch a task, we do NOT fetch the user
   @ManyToOne(() => User, user => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true }) // Exclude the user field when transforming to plain object
   user: User;
 }
