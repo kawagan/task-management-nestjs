b1dbf4d51682c00eb73e75793c4fdac2b6c90043
kawajan <kawajan2@gmail.com>
2025-09-05 18:15:10 +0200

    config environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
